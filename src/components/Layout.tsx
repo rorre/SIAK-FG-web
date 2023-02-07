212bb2c65c631361da1273ae637502e16623fd74
Rendy Arya Kemal <renrror@gmail.com>
2023-02-07 18:50:18 +0700

    Use Layout for root

---
+++ src/components/Layout.tsx
@@ -0,0 +1,69 @@
+import { JSX, useContext } from "solid-js";
+import { A } from "solid-start";
+import { AuthContext } from "./AuthContext";
+
+interface Props {
+  children: JSX.Element;
+}
+
+export default function Layout(props: Props) {
+  const { user } = useContext(AuthContext);
+  return (
+    <div class="drawer">
+      <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+      <div class="drawer-content flex flex-col">
+        <div class="w-full navbar bg-base-300">
+          <div class="flex-none lg:hidden">
+            <label for="my-drawer-3" class="btn btn-square btn-ghost">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                class="inline-block w-6 h-6 stroke-current"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
+          </div>
+          <div class="flex-1 px-2 mx-2">SIAK-FG</div>
+          <div class="flex-none hidden lg:block">
+            <ul class="menu menu-horizontal">
+              <li>
+                <A href="/">Home</A>
+              </li>
+              {user() === null ? (
+                <li>
+                  <A href="/auth">Login</A>
+                </li>
+              ) : (
+                <li>
+                  <A href="/academic">Academic</A>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+        {props.children}
+      </div>
+      <div class="drawer-side">
+        <label for="my-drawer-3" class="drawer-overlay"></label>
+        <ul class="menu p-4 w-80 bg-base-100">
+          <li>
+            <A href="/">Home</A>
+          </li>
+          <li>
+            <A href="/auth">Login</A>
+          </li>
+          <li>
+            <A href="/academic">Academic</A>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
--- src/root.tsx
+++ src/root.tsx
@@ -16,6 +16,7 @@ import {
 import "./root.css";
 import { Toaster } from "solid-toast";
 import { AuthContextProvider } from "./components/AuthContext";
+import Layout from "./components/Layout";
 
 export default function Root() {
   const location = useLocation();
@@ -35,61 +36,11 @@ export default function Root() {
           <ErrorBoundary>
             <AuthContextProvider>
               <Toaster />
-              <div class="drawer">
-                <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
-                <div class="drawer-content flex flex-col">
-                  <div class="w-full navbar bg-base-300">
-                    <div class="flex-none lg:hidden">
-                      <label for="my-drawer-3" class="btn btn-square btn-ghost">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          class="inline-block w-6 h-6 stroke-current"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M4 6h16M4 12h16M4 18h16"
-                          ></path>
-                        </svg>
-                      </label>
-                    </div>
-                    <div class="flex-1 px-2 mx-2">SIAK-FG</div>
-                    <div class="flex-none hidden lg:block">
-                      <ul class="menu menu-horizontal">
-                        <li>
-                          <A href="/">Home</A>
-                        </li>
-                        <li>
-                          <A href="/auth">Login</A>
-                        </li>
-                        <li>
-                          <A href="/academic">Academic</A>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                  <Routes>
-                    <FileRoutes />
-                  </Routes>
-                </div>
-                <div class="drawer-side">
-                  <label for="my-drawer-3" class="drawer-overlay"></label>
-                  <ul class="menu p-4 w-80 bg-base-100">
-                    <li>
-                      <A href="/">Home</A>
-                    </li>
-                    <li>
-                      <A href="/auth">Login</A>
-                    </li>
-                    <li>
-                      <A href="/academic">Academic</A>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+              <Layout>
+                <Routes>
+                  <FileRoutes />
+                </Routes>
+              </Layout>
             </AuthContextProvider>
           </ErrorBoundary>
         </Suspense>
